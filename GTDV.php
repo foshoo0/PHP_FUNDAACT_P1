5fb64de2fd7d78bbfe0e931a1a2471f45633a870
foshoo0 <167631179+foshoo0@users.noreply.github.com>
2024-05-07 21:52:39 +0800

    Add files via upload

---
+++ GTDV.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP ACTIVITY: Discriminant Calculator</title>
+</head>
+<body>
+    <form action="eq.php" method="GET">
+        <label for="A">A:</label>
+        <input type="text" placeholder="T" name="A" required>
+        <br><br>
+        <label for="B">B:</label>
+        <input type="text" placeholder="B" name="B" required>
+        <br><br>
+        <label for="C">C:</label>
+        <input type="text" placeholder="C" name="C" required>
+        <br><br>
+        <input type="submit" value="Calculate" name="calc">
+      </form>
+</body>
+</html>
